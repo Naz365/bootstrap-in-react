9ff32f344ae1e9f0899c5b597bc3642580a10d8e
Naz365 <nazmul.gub.cse@gmail.com>
2021-10-01 23:07:14 +0600

    using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
-import './App.css';
-import { useEffect,useState } from 'react';
-import { Row, Spinner } from 'react-bootstrap';
-import News from './components/News/News';
+import { useEffect, useState } from "react";
+import { Row, Spinner } from "react-bootstrap";
+import "./App.css";
+import News from "./components/News/News";
 
 function App() {
   const [news, setNews] = useState([]);
-useEffect ( ()=>{
-  fetch(' https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=a7e11e4824d64b3fba564f66702d6e3c ')
-  .then(res=> res.json())
-  .then(data => setNews(data.articles));
-}, [])
+  useEffect(() => {
+    fetch(
+      " https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=a7e11e4824d64b3fba564f66702d6e3c "
+    )
+      .then((res) => res.json())
+      .then((data) => setNews(data.articles));
+  }, []);
 
   return (
     <div className="App">
-       {
-            news.length ===0 ?
-            <Spinner animation="border" variant="success" />
-            :
-          <Row xs={1} md={2} className="g-4">
-            {
-              news.map(nw => <News news={nw}>
+      {
+        news.length ===0 ?
+        <Spinner animation="grow" />
+        :
+        <Row xs={1} md={2} className="g-4">
+        {
+          news.map(nw => <News news={nw}>
 
-              </News>)
-            }
-          
-          </Row>
+          </News>)
         }
+      
+      </Row>
 
+      }
     </div>
   );
 }
